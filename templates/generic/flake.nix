a52abcdfb45724ed27d602b4a8dc57b269ea9801
Avimitin <dev@avimit.in>
2024-05-16 15:50:41 +0800

    Make generic flake more concise
    Signed-off-by: Avimitin <dev@avimit.in>

--- templates/generic/flake.nix
+++ templates/generic/flake.nix
@@ -2,35 +2,24 @@
   description = "Generic devshell setup";
 
   inputs = {
-    # The nixpkgs
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-    # Utility functions
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = inputs@{ self, nixpkgs, flake-utils }:
     let
-      overlay = import ./overlay.nix;
-      pkgsForSys = system: import nixpkgs { inherit system; };
-      perSystem = (system:
-        let
-          pkgs = pkgsForSys system;
-        in
-        {
-          devShells.default = pkgs.mkShell {
-            buildInputs = [ ];
-          };
-
-          formatter = pkgs.nixpkgs-fmt;
-          legacyPackages = pkgs;
-        });
+      overlay = import ./nix/overlay.nix;
     in
     {
-      # Other system-independent attr
+      # System-independent attr
       inherit inputs;
       overlays.default = overlay;
-    } //
-
-    flake-utils.lib.eachDefaultSystem perSystem;
+    } // flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { overlays = [ overlay ]; inherit system; };
+      in
+      {
+        formatter = pkgs.nixpkgs-fmt;
+        legacyPackages = pkgs;
+      });
 }
